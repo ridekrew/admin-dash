70ed380c1a46bca97c371420992822d916d7c617
Austin Osborne <ffosb22@gmail.com>
2017-07-01 11:08:53 -0500

    Removed unnecessary files

--- client/src/components/views/rides.js
+++ client/src/components/views/rides.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import { Row, Col, Grid } from 'react-bootstrap';
 import request from 'superagent';
+import ReactDataGrid from 'react-data-grid';
+import update from 'immutability-helper';
 
 import './rides.css';
 
@@ -8,12 +10,32 @@ export default class Rides extends Component {
     constructor(props) {
         super(props);
         this.state = {
+            rideRequestColumns: [
+                {
+                    key: 'origin',
+                    name: 'Origin'
+                },
+                {
+                    key: 'destination',
+                    name: 'Destination'
+                },
+                {
+                    key: 'time',
+                    name: 'Time'
+                },
+                {
+                    key: 'date',
+                    name: 'Date'
+                },
+                {
+                    key: 'driver',
+                    name: 'Driver',
+                    editable: true
+                }
+            ],
             rideRequests: [],
-            rideRequestsLoaded: false,
             acceptedRides: [],
-            acceptedRidesLoaded: false,
-            rideHistory: [],
-            rideHistoryLoaded: false
+            rideHistory: []
         }
     }
 
@@ -30,12 +52,26 @@ export default class Rides extends Component {
             });
     }
 
+    rideRequestRowGetter = (i) => {
+        return this.state.rideRequests[i];
+    }
+
+    handleRideRequestUpdate = ({ fromRow, toRow, updated}) => {
+        console.log(fromRow, toRow, updated);
+        let rideRequests = this.state.rideRequests.slice();
+
+        for (let i = fromRow; i <= toRow; i++) {
+            let rowToUpdate = rideRequests[i];
+            let updatedRow = update(rowToUpdate, {$merge: updated});
+            rideRequests[i] = updatedRow;
+        }
+
+        this.setState({ 
+            rideRequests: rideRequests
+         });
+    }
+
     render() {
-        var rideRequests = this.state.rideRequests.map(rideRequest => 
-            <div key={rideRequest._id}>
-                Origin: {rideRequest.origin} -> Destination: {rideRequest.destination}
-            </div>    
-        );
 
         return(
             <Grid fluid className="rides-dash">
@@ -46,7 +82,13 @@ export default class Rides extends Component {
                     <small>These rides have not yet been accepted by drivers.</small>
                 </Row>
                 <Row>
-                    {rideRequests}
+                    <ReactDataGrid 
+                        enableCellSelect={true}
+                        rowGetter={this.rideRequestRowGetter}
+                        rowsCount={this.state.rideRequests.length}
+                        columns={this.state.rideRequestColumns}    
+                        onGridRowsUpdated={this.handleRideRequestUpdate}
+                    />
                 </Row>
                 <Row>
                     <h1>Accepted Rides</h1>
